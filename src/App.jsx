6ae7b5da5aa2202bc4bee1fd2fab42c634478713
congthanhzz04~ <106418146+congthanhzz04@users.noreply.github.com>
2025-03-09 20:55:21 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Home from './pages/Home/Home'
+import Router from './route/router'
 
 function App() {
 
   return (
     <>
-      <Home />
+      <Router/>
     </>
   )
 }
--- src/route/routes.jsx
+++ src/route/routes.jsx
@@ -1,16 +1,16 @@
 import MainLayout from '@/layout/MainLayout'
 import Home from '@/pages/home/Home'
-
+import Login from '@/pages/login/Login'
 export const publicRoutes = [
   {
     path: '/',
     element: <MainLayout />,
     children: [{ index: true, element: <Home /> }],
   },
-  //   {
-  //     path: 'login',
-  //     element: <Login />,
-  //   },
+  {
+    path: 'login',
+    element: <Login />,
+  },
   //   {
   //     path: 'register',
   //     element: <Register />,
