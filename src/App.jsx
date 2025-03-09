e0cd31da464135f1924fccf58b4069afa02ed8d4
DuyHung03 <duyhung180903@gmail.com>
2025-03-09 20:53:29 +0700

    update config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css'
-import Home from './pages/Home/Home'
+import Router from './route/router'
 
 function App() {
-
   return (
     <>
-      <Home />
+      <Router />
     </>
   )
 }
