2ee6a840a6bd98e61f1404d4a4ebad20b5c44a49
DuyHung03 <duyhung180903@gmail.com>
2025-03-10 20:48:42 +0700

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,13 @@
+import { MantineProvider } from '@mantine/core'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(<App />)
+const queryClient = new QueryClient()
+createRoot(document.getElementById('root')).render(
+  <MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </MantineProvider>
+)
