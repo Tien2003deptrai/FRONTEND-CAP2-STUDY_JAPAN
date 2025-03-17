9711d14e7f817acad5e5d3c0e092262355fb98bd
DuyHung03 <duyhung180903@gmail.com>
2025-03-17 11:36:21 +0700

    primary btn, second-btn

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -66,7 +66,7 @@ const Home = () => {
 
   return (
     <div className="container mx-auto px-6 py-8">
-      <h1 className="text-4xl font-bold text-center text-blue-700 mb-6">
+      <h1 className="text-4xl font-bold text-center text-red-600 mb-6">
         📚 Khoá học tiếng Nhật
       </h1>
       <CourseList courses={courses} />
